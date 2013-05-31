7963ab101f1ea93a0f3bb9a72f7f36b21b9284da
jlevenstein <jenelle.levenstein@gmail.com>
2013-05-31 12:59:01 -1000

    Moving Score to it's own file

--- src/othello/Score.scala
+++ src/othello/Score.scala
@@ -6,41 +6,41 @@ import scala.collection.mutable.ListBuffer
 import scala.xml.Elem
 import java.io.File
 
-case class Score(score : Float, heuristics : Map[String, Float] = Map(), maxValue : Boolean = false, minValue: Boolean = false) extends Ordered[Score] {
-    def compare(o : Score) = {
+case class Score(score: Float, heuristics: Map[String, Float] = Map(), maxValue: Boolean = false, minValue: Boolean = false) extends Ordered[Score] {
+  def compare(o: Score) = {
     score compareTo o.score
   }
-  
+
   def toStringLong = {
-    if(maxValue){
+    if (maxValue) {
       "MaxValue"
-    } else if(minValue){
+    } else if (minValue) {
       "MinValue"
     } else {
       "%s (%s)".format(score, heuristics)
-    }    	
+    }
   }
-  
+
   override def toString = {
-    if(maxValue){
+    if (maxValue) {
       "MaxValue"
-    } else if(minValue){
+    } else if (minValue) {
       "MinValue"
     } else {
       "%s".format(score)
-    }    
+    }
   }
-  def toXML : Elem = { 
-    <score value={this.toString}>
-     	{ for((heuristic, value) <- heuristics ) yield <heuristic name={heuristic} value={value.toString} />}
-    </score>	
+  def toXML: Elem = {
+    <score value={ this.toString }>
+      { for ((heuristic, value) <- heuristics) yield <heuristic name={ heuristic } value={ value.toString }/> }
+    </score>
   }
-  
-  def max(x : Score) = if(score > x.score) this else x  
-  def min(x : Score) = if(score < x.score) this else x  
+
+  def max(x: Score) = if (score > x.score) this else x
+  def min(x: Score) = if (score < x.score) this else x
 }
 
 object Score {
-  val MaxValue = Score(Float.MaxValue, maxValue=true)
-  val MinValue = Score(Float.MinValue, minValue=true)  
+  val MaxValue = Score(Float.MaxValue, maxValue = true)
+  val MinValue = Score(Float.MinValue, minValue = true)
 }
